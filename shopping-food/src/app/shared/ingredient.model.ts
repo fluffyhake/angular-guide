b93962d580fae72256190b01ca2b1856c7f3e5f2
Haakon Reppen <haakon.reppen@nhn.no>
2023-02-24 16:24:27 +0100

    Create the ingredient class - used fancy shortcut to avoid writing so much

---
+++ shopping-food/src/app/shared/ingredient.model.ts
@@ -0,0 +1,7 @@
+export class Ingredient {
+  constructor(public name: string, public amount: number) {}
+}
+// WHAT IS THIS CALLED??
+
+// 👀👀👀 https://dev.to/satansdeer/typescript-constructor-shorthand-3ibd
+// Remember this håkan
