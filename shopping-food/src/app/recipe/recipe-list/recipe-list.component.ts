43c203a45be4ce6360a41b318300dfbf242eb967
Haakon Reppen <haakon.reppen@nhn.no>
2023-02-24 16:22:27 +0100

    Import Recipe class and create some dummy-data

--- shopping-food/src/app/recipe/recipe-list/recipe-list.component.ts
+++ shopping-food/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    // This creates a new recipe based on our blueprint
+    // This calls the constructor in the recipe.model
+    new Recipe(
+      'A test Recipe',
+      'This is a test',
+      'https://bbqgrillandsmoke.com/wp-content/uploads/2022/08/Grilled-Sandwich-Recipe-864x1536.png?ezimgfmt=ng:webp/ngcb1'
+    ),
+    new Recipe(
+      'A test Recipe',
+      'This is a test',
+      'https://bbqgrillandsmoke.com/wp-content/uploads/2022/08/Grilled-Sandwich-Recipe-864x1536.png?ezimgfmt=ng:webp/ngcb1'
+    ),
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
